32ec57d1e7d96eaa23f0a9fca6a24a202a2ff9a3
Laust <laust.bonnesen@mail.com>
2021-11-22 14:47:06 +0100

    Initial commit

---
+++ src/main/java/group_g/christiansoeexamproject/ChristiansoeExamProjectApplication.java
@@ -0,0 +1,13 @@
+package group_g.christiansoeexamproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChristiansoeExamProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChristiansoeExamProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/group_g/christiansoeexamproject/ChristiansoeExamProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package group_g.christiansoeexamproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristiansoeExamProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
