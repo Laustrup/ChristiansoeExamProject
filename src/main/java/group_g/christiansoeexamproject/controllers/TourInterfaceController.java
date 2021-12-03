c5b6e12980c7f0208eb1d8bf3b9f6ae653320b0b
Geowall <rasmushr13@hotmail.com>
2021-12-03 13:01:17 +0100

    Skeleton for tourchooser

---
+++ src/main/java/group_g/christiansoeexamproject/controllers/TourInterfaceController.java
@@ -0,0 +1,32 @@
+package group_g.christiansoeexamproject.controllers;
+
+
+import group_g.christiansoeexamproject.models.Tour;
+import group_g.christiansoeexamproject.utilities.Wallet;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class TourInterfaceController {
+
+    Wallet wallet;
+
+    public TourInterfaceController(){
+
+        //TODO: Needs to take wallet in parameter, when we figure out how
+        wallet = new Wallet();
+    }
+
+    @GetMapping("/tours")
+    public ResponseEntity<List<Tour>> tourList(){
+
+        List<Tour> response = wallet.getAllTours();
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+}
