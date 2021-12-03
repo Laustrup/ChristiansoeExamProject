e6fba6bcaec9338e1ecfe38226cd18b231880862
Laust <laust.bonnesen@mail.com>
2021-12-03 11:35:40 +0100

    First update of wallet

--- src/main/java/group_g/christiansoeexamproject/utilities/Wallet.java
+++ src/main/java/group_g/christiansoeexamproject/utilities/Wallet.java
@@ -1,5 +1,6 @@
 package group_g.christiansoeexamproject.utilities;
 
+import group_g.christiansoeexamproject.models.Location;
 import group_g.christiansoeexamproject.models.Tour;
 
 import java.util.ArrayList;
@@ -9,38 +10,71 @@ import java.util.Map;
 
 public class Wallet {
 
-    private Map<String, Object> map = new HashMap<>();
+    private Map<String, Object> inbox = new HashMap<>();
 
-    public Object getMap(String key) {
-        return map.get(key);
+    // updates
+    public Map<String, Object> update() {
+
+
+
+        return inbox;
     }
 
-    public void setMap(String key, Object value) {
-        map.replace(key, value);
+    public Object getObject(String key) {
+        return inbox.get(key);
     }
 
-    public Map<String, Object> putInMap(String key, Object value) {
-        map.put(key, value);
-        return map;
+    // Returns true if key exists and replaces the key's value with method's value parameter
+    public boolean replace(String key, Object value) {
+        if (inbox.containsKey(key)) {
+            inbox.replace(key, value);
+            return true;
+        }
+        return false;
+    }
+
+    public Map<String, Object> putIn(String key, Object value) {
+        inbox.put(key, value);
+        return inbox;
     }
 
     // Returns all tours
     public List<Tour> getAllTours() {
         List<Tour> tours = new ArrayList<>();
 
+        for (int i = 0; i < inbox.size(); i++) {
+            if (inbox.containsKey("Tour"+i+1)) {tours.add((Tour) inbox.get("Tour"+i+1));}
+        }
+
         return tours;
     }
 
-    public void has(Object value) {
-        map.containsValue(value);
+    // Returns all locations
+    public List<Location> getAllLocations() {
+        List<Location> locations = new ArrayList<>();
+
+        for (int i = 0; i < inbox.size(); i++) {
+            if (inbox.containsKey("Location"+i+1)) {locations.add((Location) inbox.get("Location"+i+1));}
+        }
+
+        return locations;
     }
 
-    public void has(String key) {
-        map.containsKey(key);
+    public boolean has(Object value) {
+        return inbox.containsValue(value);
     }
 
-    public void delete(String key) {
-        map.remove(key);
+    public boolean has(String key) {
+        return inbox.containsKey(key);
+    }
+
+    // returns true if inbox contains key input and will remove its value
+    public boolean delete(String key) {
+        if (inbox.containsKey(key)) {
+            inbox.remove(key);
+            return true;
+        }
+        return false;
     }
 
 }
