b1ca9fe45206d7a2ac1356e4e4b63db3b0084b2a
Laust <laust.bonnesen@mail.com>
2021-12-03 13:00:32 +0100

    Wallet can contain and give tours and locations. Needs tests.

--- src/main/java/group_g/christiansoeexamproject/repositories/SoundRepository.java
+++ src/main/java/group_g/christiansoeexamproject/repositories/SoundRepository.java
@@ -3,5 +3,4 @@ package group_g.christiansoeexamproject.repositories;
 import group_g.christiansoeexamproject.models.Sound;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface SoundRepository extends JpaRepository<Sound, Long> {
-}
+public interface SoundRepository extends JpaRepository<Sound, Long> {}
---
+++ src/main/java/group_g/christiansoeexamproject/repositories/TourRepository.java
@@ -0,0 +1,6 @@
+package group_g.christiansoeexamproject.repositories;
+
+import group_g.christiansoeexamproject.models.Tour;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TourRepository extends JpaRepository<Tour, Long> {}
