93705b015825523983ba5994c2fd7e98bd971052
Patrick <skovskiver@gmail.com>
2021-12-07 13:59:54 +0100

    Dummy Codes updated til at virke ift. JPA entiteter og DB

--- src/main/resources/sqls/dummy_codes.sql
+++ src/main/resources/sqls/dummy_codes.sql
@@ -3,21 +3,24 @@ USE christiansoe_db;
 
 INSERT INTO animal(title,report) VALUES ("Sæl","Dette er en sæl!");
 
-INSERT INTO image(file_path,image_location,image_animal)
-VALUES ("C:\Users\Laust\IdeaProjects\ChristiansoeExamProject\src\main\resources\static\images\seal.jpeg",null,1);
+INSERT INTO location(dtype, title,report,longitude,latitude)
+VALUES ("Attraction", "Danmarks østligeste punkt","Du kan ikke finde et andet punkt i Danmark der er mere østligt end dette!",15.192853298391688,55.32029540616608);
 
-INSERT INTO sound(file_path,sound_location,sound_animal)
-VALUES ("C:\Users\Laust\IdeaProjects\ChristiansoeExamProject\src\main\resources\static\sounds\seal.wav",null,1);
+INSERT INTO location(dtype, title,report,longitude,latitude) VALUES ("Attraction", "Badebro","Dette er en badebro!",15.183442624715434,55.32128710201616);
 
-INSERT INTO location(dtype,title,report,longitude,latitude)
-VALUES ("Attraction","Danmarks østligeste punkt","Du kan ikke finde et andet punkt i Danmark der er mere østligt end dette!",15.192853298391688,55.32029540616608);
+INSERT INTO image(file_path, title, image_animal, image_location)
+VALUES ("C:\Users\Laust\IdeaProjects\ChristiansoeExamProject\src\main\resources\static\images\seal.jpeg", "billedet er her", null,1);
 
-INSERT INTO location(dtype,title,report,longitude,latitude) VALUES ("Attraction","Badebro","Dette er en badebro!",15.183442624715434,55.32128710201616);
+INSERT INTO sound(file_path, title, sound_animal, sound_location)
+VALUES ("C:\Users\Laust\IdeaProjects\ChristiansoeExamProject\src\main\resources\static\sounds\seal.wav", "Lyd her", null,1);
 
-INSERT INTO location_animals(location_id,animal_id) VALUES (1,1);
-INSERT INTO location_animals(location_id,animal_id) VALUES (2,1);
+INSERT INTO animal_locations(animals_id,locations_id) VALUES (1,1);
+INSERT INTO animal_locations(animals_id,locations_id) VALUES (1,2);
 
 INSERT INTO tour(title,report) VALUES ("Fra vest til øst","Denne tur er fra vest til øst!");
 
-INSERT INTO tour_locations(tour_id,location_id) VALUES (1,1);
-INSERT INTO tour_locations(tour_id,location_id) VALUES (1,2);
+INSERT INTO tour(title,report) VALUES ("Spøgelseturen","Du bliver hjemsøgt, hvis du tager på denne faaaarlige tur");
+INSERT INTO tour(title,report) VALUES ("Frøturen","Elsker du frølyde? Hedder du Janus? Dette er turen for dig");
+
+INSERT INTO tour_locations(tours_id,locations_id) VALUES (1,1);
+INSERT INTO tour_locations(tours_id,locations_id) VALUES (1,2);
