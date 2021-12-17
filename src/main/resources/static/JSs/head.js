b725b147ed0b0ad80d52219253fd84df414302ff
Laust <laust.bonnesen@mail.com>
2021-12-17 10:44:44 +0100

    Render frontpage again

--- src/main/resources/static/JSs/head.js
+++ src/main/resources/static/JSs/head.js
@@ -1,4 +1,8 @@
-function headAndHeaderTitles() {
-    const response = await fetch("http://localhost:8080/tours")
+async function headAndHeaderTitles() {
+    let id = sessionStorage.getItem("locationId")
+
+    const response = await fetch("http://localhost:8080//location/" + id)
     const data = await response.json()
+
+    document.getElementById("location_Title").innerText = data.title;
 }
