f9c3dcd09303896a714ad78271b2ceb2d470af55
Geowall <rasmushr13@hotmail.com>
2021-12-17 10:48:12 +0100

    Now displays animal on postpage

--- src/main/resources/static/JSs/post_fetch.js
+++ src/main/resources/static/JSs/post_fetch.js
@@ -1,3 +1,4 @@
+
 getPost()
 async function getPost() {
     let id = sessionStorage.getItem("locationId")
@@ -5,19 +6,18 @@ async function getPost() {
     const response = await fetch("http://localhost:8080/location/" + id)
     const data = await response.json()
 
+    setUpPostHeader(data)
+    setUpSoundHtml(data)
+    setUpImageHtml(data)
+    setUpAnimalHtml(data)
+}
+
+function setUpPostHeader(data){
     document.getElementById("Title").innerText = data.title
     document.getElementById("Report").innerText = data.report
+}
 
-    let imageHtml = ``
-
-    for(let i = 0; i < data.images.length; i++){
-        imageHtml +=
-            `<div class="imageItem">
-                <h1>${data.images[i].title}</h1>
-                <img src="../../../static/${data.images[i].filePath}" alt="Couldn't show image">
-            </div>`
-    }
-
+function setUpSoundHtml(data){
 
     let soundHtml = ``
     for(let i = 0; i < data.sounds.length; i++){
@@ -30,9 +30,59 @@ async function getPost() {
                 </audio>
             </div>`
     }
-
-    document.getElementById("Images").innerHTML = imageHtml
     document.getElementById("Sounds").innerHTML = soundHtml
 
-    console.log(data)
+}
+
+function setUpImageHtml(data){
+
+    let imageHtml = ``
+
+    for(let i = 0; i < data.images.length; i++){
+        imageHtml +=
+            `<div class="imageItem">
+                <h1>${data.images[i].title}</h1>
+                <img src="../../../static/${data.images[i].filePath}" alt="Couldn't show image">
+            </div>`
+    }
+
+    document.getElementById("Images").innerHTML = imageHtml
+}
+
+function setUpAnimalHtml(data){
+    let animalHtml = ``
+    for(let i = 0; i < data.animals.length; i++){
+        animalHtml +=
+            `<div class="animalItem">
+            <div class="animalHeader">
+            <h1>${data.animals[i].title}</h1>
+            <p>${data.animals[i].report}</p>
+            </div>
+            <div class="animalContent">
+            <div class="animalImages">`
+
+        for(let j = 0; j < data.animals[i].images.length; j++){
+            animalHtml +=
+                `<img src="../../../static/${data.animals[i].images[j].filePath}"
+                alt="Couldn't show image">`
+        }
+
+        animalHtml +=
+            `</div>
+            <div class="animalSounds">`
+
+        for(let j = 0; j < data.animals[i].sounds.length; j++){
+
+            animalHtml +=
+                `<audio controls>
+                    <source src="../../../static/${data.animals[i].sounds[j].filePath}" type="audio/wav">
+                </audio>`
+        }
+
+        animalHtml +=
+            `</div>
+            </div>`
+    }
+
+    document.getElementById("Animals").innerHTML = animalHtml
 }
